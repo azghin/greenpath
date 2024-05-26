cfaf596ffec9e480d91a69cd0f431bcd2355caab
azghin <azghinabdelkarim@gmail.com>
2024-05-26 21:43:15 +0100

    new updates

---
+++ src/components/clients/PlaceDetails.jsx
@@ -0,0 +1,18 @@
+function PlaceDetails(){
+    return(
+        <section id="place-details">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6"><h2>some text about the place</h2></div>
+                    <div className="col-6"> map</div>
+                </div>
+                <div className="row">
+                    <div><h2>img</h2></div>
+                    <div></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PlaceDetails
--- src/pages/client/Place.jsx
+++ src/pages/client/Place.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom"
+import PlaceDetails from "../../components/clients/PlaceDetails"
+
+
+
 function Place(){
+const Path=useLocation().pathname.split('/')
     return(
         <>
         <section id="place" className="clearfix">
@@ -7,10 +13,11 @@ function Place(){
                     img
                 </div>
                 <div className="placeIntro">
-                    <h2>in 'this place' you'll find </h2>
+                    <h2>you're now navigating <span> {Path[Path.length-2]}</span>  <br/> in <span>{Path[Path.length-1]}</span>  you'll find </h2>
                 </div>
             </div>
         </section>
+        <PlaceDetails/>
         </>
         
     )
